09a96c4cdfff58e58dfae405701d6016bee7d21b
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-09-19 20:05:52 +0200

    Flyttet html til js

---
+++ src/main_content.js
@@ -0,0 +1,27 @@
+const loadContent = window.addEventListener('load', ()=> {
+    const content = document.getElementById('content')
+
+    const frontPageBakgrunnsBilde = document.createElement('div');
+    frontPageBakgrunnsBilde.id = 'bestillBord';
+
+    const bestillBord = document.createElement('button');
+    bestillBord.textContent = 'BESTILL BORD'
+
+    frontPageBakgrunnsBilde.appendChild(bestillBord);
+
+    const omOss = document.createElement('div');
+    omOss.id = 'omOss';
+
+    const vaarLocationBilde = document.createElement('img');
+    vaarLocationBilde.src = './Images/bravo-location.png';
+    vaarLocationBilde.alt = 'Google Maps'
+
+    const omOssTekst = document.createElement('div');
+    omOssTekst.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat accusamus, nihil sit optio vero eius quis, perspiciatis eaque et, quos quae consectetur est ut vitae ipsum voluptatibus exercitationem vel atque!';
+
+    omOss.append(vaarLocationBilde, omOssTekst)
+
+    content.append(frontPageBakgrunnsBilde, omOss)
+})
+
+export {loadContent};
