5a69a339efc2aecda00c5b33324ab7bd24a041d5
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-09-19 20:10:40 +0200

    Gjorde loadContent mer lesbar

--- src/main_content.js
+++ src/main_content.js
@@ -1,11 +1,11 @@
-const loadContent = window.addEventListener('load', ()=> {
-    const content = document.getElementById('content')
+function loadContent() {
+    const content = document.getElementById('content');
 
     const frontPageBakgrunnsBilde = document.createElement('div');
     frontPageBakgrunnsBilde.id = 'bestillBord';
 
     const bestillBord = document.createElement('button');
-    bestillBord.textContent = 'BESTILL BORD'
+    bestillBord.textContent = 'BESTILL BORD';
 
     frontPageBakgrunnsBilde.appendChild(bestillBord);
 
@@ -14,14 +14,17 @@ const loadContent = window.addEventListener('load', ()=> {
 
     const vaarLocationBilde = document.createElement('img');
     vaarLocationBilde.src = './Images/bravo-location.png';
-    vaarLocationBilde.alt = 'Google Maps'
+    vaarLocationBilde.alt = 'Google Maps';
 
     const omOssTekst = document.createElement('div');
     omOssTekst.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat accusamus, nihil sit optio vero eius quis, perspiciatis eaque et, quos quae consectetur est ut vitae ipsum voluptatibus exercitationem vel atque!';
 
-    omOss.append(vaarLocationBilde, omOssTekst)
+    omOss.append(vaarLocationBilde, omOssTekst);
 
-    content.append(frontPageBakgrunnsBilde, omOss)
-})
+    content.append(frontPageBakgrunnsBilde, omOss);
+}
 
-export {loadContent};
+// Kall funksjonen når vinduet er lastet
+window.addEventListener('load', loadContent);
+
+export { loadContent };
