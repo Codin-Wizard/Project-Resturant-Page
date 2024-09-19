0494b6b04ac16522f3efa8a4d9c7ff3aa762e1ac
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-09-19 19:11:34 +0200

    Project resturant page

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      { //https://webpack.js.org/loaders/css-loader/
+        test: /\.css$/, 
+        use: ["style-loader"]
+    },
+
+    { //https://webpack.js.org/loaders/css-loader/
+        test: /\.css$/, 
+        loader: "css-loader",
+        options: {
+            url: true,
+            esModule: false
+        }
+    },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
