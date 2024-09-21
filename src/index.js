c25b90030e4b85f3618392f81aad977400cdf6ab
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-09-21 10:17:33 +0200

    Fanen som er aktiv har farge på knappen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import "./style.css"
 
 import {clearContent, loadFrontPageContent, loadBestillBord } from "./main_content"
 
-loadFrontPageContent()
+loadFrontPageContent();
+
+//En funksjon for å fjerne klassen "active" fra alle knappene og legge den på den aktive knappen
+function setActiveButton(knapp) {
+        // Fjern "active" fra alle knapper
+        document.querySelectorAll('.nav-knapp').forEach(button => {
+            button.classList.remove('active');
+        });
+        // Legg til "active" på den klikkede knappen
+        knapp.classList.add('active');
+}
 
 const index = document.querySelector('.home');
 const bestillBordKnapp = document.querySelector('.bestillBordKnapp');
@@ -14,13 +24,15 @@ const menyKnapp = document.querySelector('.menyKnapp');
 index.addEventListener('click', ()=> {
     clearContent();
     loadFrontPageContent();
+    setActiveButton(index);
 })
 
 // Event listener for "BESTILL BORD"
-bestillBord.forEach(knapp => {
+bestillBordKnapp.forEach(knapp => {
     knapp.addEventListener('click', () => {
         clearContent();
         loadBestillBord();
+        setActiveButton(bestillBordKnapp);
     });
 });
 
@@ -28,17 +40,20 @@ bestillBord.forEach(knapp => {
 tiderKnapp.addEventListener('click', () => {
     clearContent();
     loadTider();
+    setActiveButton(tiderKnapp);
 });
 
 // Event listener for "GAVEKORT"
 gavekortKnapp.addEventListener('click', () => {
     clearContent();
     loadGavekort();
+    setActiveButton(gavekortKnapp);
 });
 
 // Event listener for "MENY"
 menyKnapp.addEventListener('click', () => {
     clearContent();
     loadMeny();
+    setActiveButton(menyKnapp);
 });
 
